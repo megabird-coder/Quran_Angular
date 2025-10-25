7fe042cc9bf4891bfcd5957734df8aa6fe4ba2d8
megabird-coder <omarcadoret94@gmail.com>
2025-10-25 19:53:30 -0400

    ajout barre de recherche

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  searchQuery: string = '';
 
+  onSearch() {
+    console.log(this.searchQuery);
+  }
 }
